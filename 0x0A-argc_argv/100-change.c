bac8b6113d452587a2ec071f2be6228988117bdd
M Dederi <mjdederi01@gmail.com>
2022-11-06 19:46:37 -0800

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,7 +7,6 @@
 /**
  * coinConverter - Helper function that does all the mathematics
  * @i: Passed in variable from main for calculations
- * Return: The number of coins needed minimum for the passed in variable
  */
 
 int coinConverter(int i);
@@ -45,6 +44,11 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
+/**
+ * coinConverter - Helper function that does all the mathematics
+ * @i: Passed in variable from main for calculations
+ * Return: The number of coins needed minimum for the passed in variable
+ */
 int coinConverter(int i)
 {
 	int count = 0;
