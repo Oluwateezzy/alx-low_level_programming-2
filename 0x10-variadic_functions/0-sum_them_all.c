36a85b0f4e61994c9d64810ffd82cfad8882db93
M Dederi <mjdederi01@gmail.com>
2022-11-17 17:47:50 -0800

    Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,29 +1,26 @@
-#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers given as parameters
- * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
+ *
+ * Return: sum of its parameters.
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
+	va_list valist;
 	unsigned int i;
-	va_list list;
+	int sum = 0;
 
-	va_start(list, n);
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
-	{
-		if (!separator)
-			printf("%d", va_arg(list, int));
-		else if (separator && i == 0)
-			printf("%d", va_arg(list, int));
-		else
-			printf("%s%d", separator, va_arg(list, int));
-	}
+		sum += va_arg(valist, int);
 
-	va_end(list);
+	va_end(valist);
 
-	printf("\n");
+	return (sum);
 }
